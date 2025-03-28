3140450744df289069f4731b2f3671ac08549b8f
Gleice Avelino <gleiceavelinor@gmail.com>
2025-03-28 00:27:01 -0300

    Implementei o script multi linguas

--- hello.py
+++ hello.py
@@ -30,7 +30,7 @@ if current_language == "pt_BR":
     msg = "Olá, Mundo!"
 elif current_language == "it_IT":
     msg = "Ciao, Mondo"
-elif current_language == ""es_SP":
+elif current_language == "es_SP":
     msg = "Holla, Mundo!"
 elif current_language == "fr_FR":
     msg = "Bonjour Monde"
